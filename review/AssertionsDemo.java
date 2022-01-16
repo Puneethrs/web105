378054b6b6032e1d32bd3a09095c9337f0b79b77
Puneethrs <97144391+Puneethrs@users.noreply.github.com>
2022-01-16 21:21:17 +0530

    Update AssertionsDemo.java

--- review/AssertionsDemo.java
+++ review/AssertionsDemo.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-
-auto{}
-ok pa
+ /True
 
 
 public class AssertionsDemo {
